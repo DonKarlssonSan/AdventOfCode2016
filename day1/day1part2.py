bf8540621e2703611b5bee01cac083a4957741ac
Johan Karlsson <DonKarlssonSan@gmail.com>
2017-12-10 00:37:00 +0100

    Add solutions for day1

---
+++ day1/day1part2.py
@@ -0,0 +1,31 @@
+import math
+import cmath
+
+a90 = math.pi/2
+def walk(instructions):
+    angle = a90
+    pos = complex(0, 0)
+    visitedPositions = []
+    firstVisitedTwice = None
+    for instruction in instructions.split(', '):
+        turn = instruction[0]
+        if turn == 'L':
+            angle += a90
+        elif turn == 'R':
+            angle -= a90
+        length = int(instruction[1:])
+        delta = cmath.rect(1, angle)
+        for i in range(0, length):
+            pos += delta
+            if pos in visitedPositions:
+                firstVisitedTwice = pos
+                break
+            visitedPositions.append(pos)
+        if firstVisitedTwice:
+            break
+
+    blocksAway = abs(pos.real) + abs(pos.imag)
+    print('Blocks away: ' + str(blocksAway) + ' ' + str(pos))
+
+walk('R8, R4, R4, R8')
+walk('R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2, R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190, R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2, R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4, R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1, R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4, R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5, R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1, R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4, L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1')
